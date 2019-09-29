817d32856a2246f3cb34f58b051b8618b9cecc59
Kamyar Inanloo <kamyar1979@gmail.com>
2019-09-29 14:26:12 +0330

    fix: revert deserialization

--- catalyst/dispatcher/deserializers.py
+++ catalyst/dispatcher/deserializers.py
@@ -10,17 +10,14 @@ from catalyst.dispatcher import deserialization_handler
 
 @deserialization_handler(MimeTypes.JSON)
 def deserialize_JSON(data: str) -> Any:
-    if data:
-        return (rapidjson.loads(data, datetime_mode=rapidjson.DM_ISO8601) or None)
+    return rapidjson.loads(data, datetime_mode=rapidjson.DM_ISO8601) if data else {}
 
 
 @deserialization_handler(MimeTypes.MessagePack)
 def deserialize_MsgPack(data: bytes) -> Any:
-    if data:
-        return (umsgpack.loads(data) or None)
+    return umsgpack.loads(data) if data else {}
 
 
 @deserialization_handler(MimeTypes.CBOR)
 def deserliaize_CBOR(data: bytes) -> Any:
-    if data:
-        return (cbor.loads(data) or None)
+    return cbor.loads(data) if data else {}
