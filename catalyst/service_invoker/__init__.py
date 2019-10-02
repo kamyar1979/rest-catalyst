67b9d8b2213e49949664d1172bc6fdd4b393a12e
Kamyar Inanloo <kamyar1979@gmail.com>
2019-10-02 10:42:41 +0330

    fix: SSL verification disabled for read swagger

--- catalyst/service_invoker/__init__.py
+++ catalyst/service_invoker/__init__.py
@@ -1,4 +1,4 @@
-from typing import Dict, Coroutine, Any
+from typing import Dict
 from catalyst.service_invoker.swagger import get_swagger_operations
 import aiohttp
 from catalyst.service_invoker.types import ParameterInfo, RestfulOperation
@@ -8,7 +8,7 @@ base_url: str = ''
 
 async def read_swagger(url) -> str:
     async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
+        async with session.get(url, verify_ssl=False) as response:
             assert response.status == 200
             return await response.text()
 
