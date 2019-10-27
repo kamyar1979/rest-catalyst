95c7feda2e9d67a05a5f11c304f4784aab733c5b
Kamyar Inanloo <kamyar1979@gmail.com>
2019-10-27 19:33:47 +0330

    fix: Geometry types fixed

--- catalyst/dispatcher/type_handlers.py
+++ catalyst/dispatcher/type_handlers.py
@@ -1,51 +1,16 @@
 import base64
-from dataclasses import is_dataclass
 from datetime import datetime, date, time
 from enum import Enum
 from typing import Any, AnyStr, Tuple, TypeVar, Type
 
-import sqlalchemy
-from geojson.geometry import Geometry
-from sqlalchemy import Binary
-from sqlalchemy.orm import RelationshipProperty
+import geojson
+import rapidjson
+from shapely.geometry import shape
+from shapely.geometry.base import BaseGeometry
 
 from catalyst.constants import DEFAULT_CHARSET
 from catalyst.dispatcher import type_handler
-from catalyst.constants import SRID
 
-
-def adapt_field_type(column, value):
-    """
-    Adapt field type from Python types into RDBMS types
-    :param column: SqlAlchemy Column
-    :param value: The Python value to fill in database
-    :return:
-    """
-    if type(column.type) == Geometry:
-        geo_type = column.type.geometry_type
-        if geo_type == 'POINT':
-            return f'SRID=${SRID};POINT(${value[0]} ${value[1]})'
-    elif type(column.type) == Binary:
-        return base64.b64encode(value)
-    return value
-
-
-def create_model(alchemy_model, **kwargs):
-    """
-    Creates a model object form items dictionary
-    :param alchemy_model: SqlAlchemy model Type
-    :param kwargs: Items dictionary
-    :return: Model object
-    """
-    model = {}
-    for k in sqlalchemy.inspect(alchemy_model).attrs:
-        if kwargs.get(k.key) is not None:
-            column = getattr(alchemy_model, k.key)
-            if hasattr(column, 'type'):
-                model[k.key] = adapt_field_type(column, kwargs[k.key])
-    if hasattr(alchemy_model, 'registered_date'):
-        setattr(alchemy_model, 'registered_date', datetime.now())
-    return alchemy_model(**model)
 
 
 @type_handler
@@ -117,15 +82,6 @@ def parse_enum(val: Any, requested_type: Type[E]) -> Enum:
         raise ValueError('Not a valid enum value')
 
 
-@type_handler
-def parse_object(data: Any, requested_type: type) -> object:
-    if hasattr(requested_type, '__table__'):
-        params = {attr.key: data.get(attr.key) for attr in
-                  sqlalchemy.inspect(requested_type).attrs
-                  if
-                  type(attr) != RelationshipProperty}
-        return create_model(requested_type, **params)
-    elif is_dataclass(requested_type):
-        return requested_type(**data)
 
-
+def parse_geometry(val: Any) -> BaseGeometry:
+    return shape(geojson.loads(rapidjson.dumps(val)))
