8a6fc1c4a1fca0b78ddd48c51d5292fdca7df985
Kamyar Inanloo <kamyar1979@gmail.com>
2019-12-03 09:20:03 +0330

    fix: default values for pool parameters

--- catalyst/broker/pool.py
+++ catalyst/broker/pool.py
@@ -18,12 +18,11 @@ pool_recycle_period: int = 900
 last_pool_init: datetime = datetime.utcnow()
 
 
-
 def renew_pool():
     global last_pool_init, connections
     if (datetime.utcnow() - last_pool_init).seconds >= pool_recycle_period:
         connections = [pika.BlockingConnection(pika.connection.URLParameters(connection_url))
-                            for i in range(pool_size)]
+                       for i in range(pool_size)]
 
     last_pool_init = datetime.utcnow()
 
@@ -45,4 +44,3 @@ def publish_event(exchange_name: str, routing_key: str, data: Any):
                                   routing_key=routing_key,
                                   body=raw_serialize(data, serialization_format),
                                   properties=pika.BasicProperties(content_type=serialization_format))
-
