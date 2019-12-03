8ced24e8ba1cf710ce215bd1eb8dc12bd424165d
Kamyar Inanloo <kamyar1979@gmail.com>
2019-12-03 08:55:42 +0330

    feat: Broker pool recycle

--- catalyst/broker/__init__.py
+++ catalyst/broker/__init__.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import Optional
 
 import pika
@@ -5,10 +6,13 @@ import pika
 from . import pool
 
 
-def register_broker(url: str, pool_size: int = 10, format: Optional[str] = None):
+def register_broker(url: str,
+                    pool_size: int = 10,
+                    format: Optional[str] = None,
+                    pool_recycle_period: Optional[int] = None):
     if format:
         pool.serialization_format = format
     pool.connection_url = url
     pool.pool_size = pool_size
-    pool.connections = [pika.BlockingConnection(pika.connection.URLParameters(url))
-                        for i in range(pool_size)]
+    pool.pool_recycle_period = pool_recycle_period
+    pool.renew_pool()
