edea9069463d4f0e6a287a4d47eefa6c20119a9b
Kamyar Inanloo <kamyar1979@gmail.com>
2021-05-29 12:01:35 +0430

    feat: Add new column type for Hy expression

--- catalyst/dsl/custom_column_type.py
+++ catalyst/dsl/custom_column_type.py
@@ -1,5 +1,7 @@
 from catalyst.dsl.sexp_json import to_json, from_json
 from sqlalchemy.sql.type_api import UserDefinedType
+import hy
+import rapidjson
 
 
 class SExpression(UserDefinedType):
@@ -12,3 +14,31 @@ class SExpression(UserDefinedType):
 
     def result_processor(self, dialect, coltype):
         return from_json
+
+class HyLangExpression(UserDefinedType):
+
+    def get_col_spec(self):
+        return "Hy-Expression"
+
+    def bind_processor(self, dialect):
+        def process(value):
+
+            def process_expr(expr_list):
+                for expr in expr_list:
+                    if isinstance(expr, hy.models.HySymbol) or \
+                            isinstance(expr, hy.models.HyString) or \
+                            isinstance(expr, hy.models.HyKeyword):
+                        yield f'"{expr}"'
+                    elif isinstance(expr, hy.models.HyExpression):
+                        yield process_expr(expr)
+                    else:
+                        yield expr.real
+            return rapidjson.dumps(process_expr(value))
+
+        return process
+
+    def result_processor(self, dialect, coltype):
+        def process(value):
+            return hy.read_str(from_json(value))
+
+        return process
